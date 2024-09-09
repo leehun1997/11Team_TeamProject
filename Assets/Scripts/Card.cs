af97bceb03fdafdf89438e5fb33a5ff53bc84745
new-Tower-of-Babel <net6994@gmail.com>
2024-09-09 16:24:13 +0900

    [Complex]Findrtan
    Findrtan full

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int idx = 0;
+
+    public GameObject front;
+    public GameObject back;
+
+    public Animator anim;
+
+    AudioSource audioSource;
+    public AudioClip clip;
+
+    public SpriteRenderer frontImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Setting(int number)
+    {
+        idx = number;
+        frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}");
+    }
+
+    public void OpenCard()
+    {
+        if (Time.timeScale != 0f)
+        {
+            audioSource.PlayOneShot(clip);
+            anim.SetBool("isOpen", true);
+            front.SetActive(true);
+            back.SetActive(false);
+
+            if (GameManager.Instance.firstCard == null)
+            {
+                GameManager.Instance.firstCard = this;
+            }
+            else
+            {
+                GameManager.Instance.secondCard = this;
+                GameManager.Instance.Matched();
+            }
+
+        }
+    }
+    public void DestroyCard()
+    {
+        Invoke("DestroyCardInvoke", 1.0f);
+    }
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke", 0.5f);
+    }
+    void CloseCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
+    }
+}
