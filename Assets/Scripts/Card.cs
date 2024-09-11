56f8f523454b8009fc7ff52d6737cb47d2a97808
bc5546 <bc12345653@gmail.com>
2024-09-11 16:18:10 +0900

    [Complex] edit picture and change into image

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
@@ -16,7 +17,7 @@ public class Card : MonoBehaviour
     AudioSource audioSource;
     public AudioClip clip;
 
-    public SpriteRenderer frontImage;
+    public Image frontImage;
     // Start is called before the first frame update
     void Start()
     {
