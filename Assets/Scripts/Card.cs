9976ad84327b8077fbf1be2d077f21eac8766d2a
bc5546 <bc12345653@gmail.com>
2024-09-11 17:01:05 +0900

    Revert "[Complex] change into image"
    This reverts commit 56f8f523454b8009fc7ff52d6737cb47d2a97808.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
@@ -17,7 +16,7 @@ public class Card : MonoBehaviour
     AudioSource audioSource;
     public AudioClip clip;
 
-    public Image frontImage;
+    public SpriteRenderer frontImage;
     // Start is called before the first frame update
     void Start()
     {
